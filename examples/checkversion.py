73faf153b4e08ade51003a8a4c03b47092f5715b
Sourcery AI <>
2023-07-01 13:28:24 +0000

    'Refactored by Sourcery'

--- examples/checkversion.py
+++ examples/checkversion.py
@@ -9,32 +9,32 @@ def main():
     try:
         print(f"libarchive version {version_details()}")
     except ArchiveError:
-        print(f"can not find libarchive")
+        print("can not find libarchive")
 
     try:
         print(f"zlib version {zlib_version()}")
     except ArchiveError:
-        print(f"can not find zlib")
+        print("can not find zlib")
 
     try:
         print(f"liblzma version {liblzma_version()}")
     except ArchiveError:
-        print(f"can not find liblzma")
+        print("can not find liblzma")
 
     try:
         print(f"bzip version {bzlib_version()}")
     except ArchiveError:
-        print(f"can not find libbzip")
+        print("can not find libbzip")
 
     try:
         print(f"lz4 version {liblz4_version()}")
     except ArchiveError:
-        print(f"can not find lz4")
+        print("can not find lz4")
 
     try:
         print(f"zstd version {libzstd_version()}")
     except ArchiveError:
-        print(f"can not find zstd")
+        print("can not find zstd")
 
 
 if __name__ == "__main__":
